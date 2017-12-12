1ab1da8052a6e85a15252b11d297171c5687ac8e
Bill Sorensen <baldgoth@gmail.com>
2017-12-11 21:18:47 -0600

    Refactor routing into App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import BasicInfoContainer from '../containers/BasicInfoContainer';
+import Attributes from './Attributes';
+import Skills from './Skills';
 
 const App = () => (
   <div>
     <h2>Embracer</h2>
     <BasicInfoContainer />
+    <Route exact path="/" component={Attributes} />
+    <Route path="/skills" component={Skills} />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from './store/configureStore';
 import './styles/index.css';
 import 'react-select/dist/react-select.css';
 import App from './components/App';
-import Attributes from './components/Attributes';
-import Skills from './components/Skills';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore();
@@ -15,11 +13,7 @@ const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <div>
-        <App />
-        <Route exact path="/" component={Attributes} />
-        <Route path="/skills" component={Skills} />
-      </div>
+      <App />
     </Router>
   </Provider>,
   document.getElementById('root')
