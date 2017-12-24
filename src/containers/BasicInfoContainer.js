3fb127b159678b0d72a2a931c879e8a4bb6d4b33
Bill Sorensen <baldgoth@gmail.com>
2017-12-24 16:46:12 -0600

    Minor refactoring

--- src/containers/BasicInfoContainer.js
+++ src/containers/BasicInfoContainer.js
@@ -3,11 +3,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/characterCreationActions';
 import BasicInfo from '../components/BasicInfo';
 
-const mapStateToProps = state => {
-  return {
-    character: state.character
-  };
-};
+const mapStateToProps = state => ({
+  character: state.character
+});
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(actions, dispatch)
