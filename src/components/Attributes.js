9058f3f72fd7cfe84076eacdc1a24516e722f5c6
Bill Sorensen <baldgoth@gmail.com>
2017-12-30 22:43:25 -0600

    Refactor Trait component

--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -6,23 +6,15 @@ import Trait from './Trait';
 const maxDots = 10;
 const rankDots = [7, 5, 3];
 
-const buildHandleRankChange = (trait, setRank) => e => {
-  const dotsFromRank = parseInt(e.target.value, 10);
-  setRank('attributes', trait, dotsFromRank);
-};
-
 class Attributes extends Component {
   static propTypes = {
     attributes: PropTypes.object.isRequired,
     setRank: PropTypes.func.isRequired
   };
 
-  handlePhysicalRankChange = buildHandleRankChange(
-    'physical',
-    this.props.setRank
-  );
-  handleSocialRankChange = buildHandleRankChange('social', this.props.setRank);
-  handleMentalRankChange = buildHandleRankChange('mental', this.props.setRank);
+  handleRankChange = (trait, dotsFromRank) => {
+    this.props.setRank('attributes', trait, dotsFromRank);
+  };
 
   render() {
     const attributes = this.props.attributes;
@@ -31,25 +23,28 @@ class Attributes extends Component {
       <div>
         <h3>Attributes</h3>
         <Trait
-          name="Physical"
+          name="physical"
+          displayName="Physical"
           maxDots={maxDots}
           rankDots={rankDots}
           traitState={attributes.physical}
-          onChange={this.handlePhysicalRankChange}
+          onRankChange={this.handleRankChange}
         />
         <Trait
-          name="Social"
+          name="social"
+          displayName="Social"
           maxDots={maxDots}
           rankDots={rankDots}
           traitState={attributes.social}
-          onChange={this.handleSocialRankChange}
+          onRankChange={this.handleRankChange}
         />
         <Trait
-          name="Mental"
+          name="mental"
+          displayName="Mental"
           maxDots={maxDots}
           rankDots={rankDots}
           traitState={attributes.mental}
-          onChange={this.handleMentalRankChange}
+          onRankChange={this.handleRankChange}
         />
         <Link to="/skills">Skills</Link>
       </div>
--- src/components/Trait.js
+++ src/components/Trait.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Dots from './Dots';
 import Rank from './Rank';
 import dotSelector from '../utils/dotSelector';
 
-const Trait = ({ name, maxDots, rankDots, traitState, onChange }) => (
-  <div>
-    {name} <Dots level={dotSelector(traitState)} max={maxDots} />
-    <Rank
-      dots={rankDots}
-      dotValue={traitState.dotsFromRank}
-      onChange={onChange}
-    />
-  </div>
-);
+class Trait extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    displayName: PropTypes.string.isRequired,
+    maxDots: PropTypes.number.isRequired,
+    rankDots: PropTypes.arrayOf(PropTypes.number).isRequired,
+    traitState: PropTypes.object.isRequired,
+    onRankChange: PropTypes.func.isRequired
+  };
 
-Trait.propTypes = {
-  name: PropTypes.string.isRequired,
-  maxDots: PropTypes.number.isRequired,
-  rankDots: PropTypes.arrayOf(PropTypes.number).isRequired,
-  traitState: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired
-};
+  handleRankChange = e => {
+    const dotsFromRank = parseInt(e.target.value, 10);
+    this.props.onRankChange(this.props.name, dotsFromRank);
+  };
+
+  render() {
+    const { displayName, maxDots, rankDots, traitState } = this.props;
+
+    return (
+      <div>
+        {displayName} <Dots level={dotSelector(traitState)} max={maxDots} />
+        <Rank
+          dots={rankDots}
+          dotValue={traitState.dotsFromRank}
+          onChange={this.handleRankChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default Trait;
