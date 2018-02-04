4eb866ee21a30903d1966adba2bb45c980a90dd6
Bill Sorensen <baldgoth@gmail.com>
2018-02-04 11:17:39 -0600

    Update skills reducer to purchase dots

--- src/reducers/skillsReducer.js
+++ src/reducers/skillsReducer.js
@@ -3,15 +3,36 @@ import * as types from '../constants/actionTypes';
 import { setDotsFromStartingDots } from '../utils/categoryStarter';
 
 export default (state = initialState.character.skills, action) => {
+  let category, trait, startingDots;
+
   switch (action.type) {
     case types.SET_STARTING_DOTS:
-      const { category, trait, startingDots } = action.payload;
+      ({ category, trait, startingDots } = action.payload);
 
       if (category !== 'skills') {
         return state;
       }
 
       return setDotsFromStartingDots(state, trait, startingDots);
+    case types.PURCHASE_DOT:
+      ({ category, trait } = action.payload);
+
+      if (category !== 'skills') {
+        return state;
+      }
+
+      const matchingTrait = state[trait];
+
+      const previousDotsPurchased = matchingTrait
+        ? matchingTrait.dotsPurchased || 0
+        : 0;
+
+      // It is legal to spread undefined.
+      return {
+        ...state,
+        [trait]: { ...matchingTrait, dotsPurchased: previousDotsPurchased + 1 }
+      };
+
     default:
       return state;
   }
