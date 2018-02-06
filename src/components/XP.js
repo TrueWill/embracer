8fb908fa856db5362cb9a89ef1b1d0db67835d12
Bill Sorensen <baldgoth@gmail.com>
2018-02-05 19:54:43 -0600

    Negative XP displays in red

--- src/components/XP.js
+++ src/components/XP.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const XP = ({ spent, gainedFromFlaws, available }) => (
-  <div>
-    <h3>XP</h3>
-    <div>Spent: {spent}</div>
-    <div>Gained from Flaws: {gainedFromFlaws}</div>
-    <div>Available: {available}</div>
-  </div>
-);
+const XP = ({ spent, gainedFromFlaws, available }) => {
+  const availableClass = classNames({ negativePoints: available < 0 });
+
+  return (
+    <div>
+      <h3>XP</h3>
+      <div>Spent: {spent}</div>
+      <div>Gained from Flaws: {gainedFromFlaws}</div>
+      <div>
+        Available: <span className={availableClass}>{available}</span>
+      </div>
+    </div>
+  );
+};
 
 XP.propTypes = {
   spent: PropTypes.number.isRequired,
