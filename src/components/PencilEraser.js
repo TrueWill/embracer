3a64e769acee54ccc88770bb3422ccbf49b6e7ba
Bill Sorensen <baldgoth@gmail.com>
2018-02-09 18:54:01 -0600

    Add action to pencil/eraser

--- src/components/PencilEraser.js
+++ src/components/PencilEraser.js
@@ -8,20 +8,26 @@ const pencilTooltipPrefix = 'Pencil mode (add dots)';
 const eraserTooltipPrefix = 'Eraser mode (remove dots)';
 const tooltipSuffix = ' (click to toggle)';
 
-const PencilEraser = ({ isEraser }) => {
+const PencilEraser = ({ isEraser, togglePencilEraserMode }) => {
   const className = isEraser ? eraserClass : pencilClass;
   const tooltip =
     (isEraser ? eraserTooltipPrefix : pencilTooltipPrefix) + tooltipSuffix;
 
   return (
-    <button type="button" className="btn btn-default" title={tooltip}>
+    <button
+      type="button"
+      className="btn btn-default"
+      title={tooltip}
+      onClick={togglePencilEraserMode}
+    >
       <i className={className} aria-hidden="true" />
     </button>
   );
 };
 
 PencilEraser.propTypes = {
-  isEraser: PropTypes.bool.isRequired
+  isEraser: PropTypes.bool.isRequired,
+  togglePencilEraserMode: PropTypes.func.isRequired
 };
 
 export default PencilEraser;
