01051f8b49f5b4342db63d7d7d21ce6ce7e7211b
Bill Sorensen <baldgoth@gmail.com>
2018-02-11 11:09:18 -0600

    Remove Service Worker
    Requires HTTPS.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import configureStore from './store/configureStore';
 import './styles/index.css';
 import 'react-select/dist/react-select.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore();
 
@@ -18,5 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
