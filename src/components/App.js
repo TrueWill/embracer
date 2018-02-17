34bed0689ba2c5ac4f15dfb4e2460f2d0bc18ab1
Bill Sorensen <baldgoth@gmail.com>
2018-02-16 20:17:15 -0600

    App changes to test morality

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import MeritsFlawsContainer from '../containers/MeritsFlawsContainer';
 import XPContainer from '../containers/XPContainer';
 import BloodContainer from '../containers/BloodContainer';
 import PencilEraserContainer from '../containers/PencilEraserContainer';
+import Morality from '../components/Morality';
 
 const skills = (
   <TraitCategoryContainer
@@ -47,6 +48,15 @@ const meritsFlaws = (
   </div>
 );
 
+const morality = (
+  <Morality
+    path="Humanity"
+    level={5}
+    maxDots={6}
+    purchaseOrUnpurchaseDot={() => console.log('click')}
+  />
+);
+
 const App = () => (
   <div>
     <h2>Embracer</h2>
@@ -59,12 +69,14 @@ const App = () => (
     <Link to="/skills">Skills</Link> |
     <Link to="/backgrounds">Backgrounds</Link> |
     <Link to="/disciplines">Disciplines</Link> |
-    <Link to="/merits_flaws">Merits / Flaws</Link>
+    <Link to="/merits_flaws">Merits / Flaws</Link> |
+    <Link to="/morality">Morality</Link>
     <Route exact path="/" component={AttributesContainer} />
     <Route path="/skills" render={() => skills} />
     <Route path="/backgrounds" render={() => backgrounds} />
     <Route path="/disciplines" render={() => disciplines} />
     <Route path="/merits_flaws" render={() => meritsFlaws} />
+    <Route path="/morality" render={() => morality} />
   </div>
 );
 
