d4f3d050f4dae69fe9979bd439fe96929c2f540f
Bill Sorensen <baldgoth@gmail.com>
2018-03-03 16:54:36 -0600

    Add tests of flaws reducer

---
+++ src/reducers/flawsReducer.test.js
@@ -0,0 +1,66 @@
+import deepFreeze from 'deep-freeze';
+import * as actions from '../actions/characterCreationActions';
+import initialState from './initialState';
+import reducer from './flawsReducer';
+
+deepFreeze(initialState.character.flaws);
+
+it('should initialize', () => {
+  const action = { type: '@@INIT' };
+
+  const nextState = reducer(undefined, action);
+
+  expect(nextState).toBe(initialState.character.flaws);
+});
+
+it('should add flaw, preserving existing', () => {
+  const state = [
+    {
+      name: 'Addiction',
+      points: 2
+    }
+  ];
+
+  deepFreeze(state);
+
+  const action = actions.addFlaw('Beacon of the Unholy', 3);
+
+  const nextState = reducer(state, action);
+
+  expect(nextState).toEqual([
+    {
+      name: 'Addiction',
+      points: 2
+    },
+    {
+      name: 'Beacon of the Unholy',
+      points: 3
+    }
+  ]);
+});
+
+it('should remove flaw', () => {
+  const state = [
+    {
+      name: 'Addiction',
+      points: 2
+    },
+    {
+      name: 'Beacon of the Unholy',
+      points: 3
+    }
+  ];
+
+  deepFreeze(state);
+
+  const action = actions.removeFlaw('Beacon of the Unholy');
+
+  const nextState = reducer(state, action);
+
+  expect(nextState).toEqual([
+    {
+      name: 'Addiction',
+      points: 2
+    }
+  ]);
+});
