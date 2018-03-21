2ddb32b1ab8e2719867f0c70e2edc7223f31f118
Bill Sorensen <baldgoth@gmail.com>
2018-03-20 22:11:13 -0500

    Add map keys to array function

--- src/utils/mapUtils.js
+++ src/utils/mapUtils.js
@@ -6,3 +6,15 @@ export const arrayToMap = a => {
   a.forEach(e => result.set(e[0], e[1]));
   return result;
 };
+
+// "Polyfill" for IE's lack of support for Map.prototype.keys().
+// Transforms a Map into an array of keys.
+export const mapKeysToArray = m => {
+  const result = [];
+
+  m.forEach((value, key) => {
+    result.push(key);
+  });
+
+  return result;
+};
--- src/utils/mapUtils.test.js
+++ src/utils/mapUtils.test.js
@@ -1,5 +1,5 @@
 import deepFreeze from 'deep-freeze';
-import { arrayToMap } from './mapUtils';
+import { arrayToMap, mapKeysToArray } from './mapUtils';
 
 describe('arrayToMap', () => {
   it('should create map from 2D array', () => {
@@ -18,5 +18,28 @@ describe('arrayToMap', () => {
     const result = arrayToMap([]);
 
     expect(result.size).toBe(0);
+  });
+});
+
+describe('mapKeysToArray', () => {
+  it('should create array from map keys in order', () => {
+    const map = new Map();
+    map.set('a', 'value1');
+    map.set('c', 'value2');
+    map.set('1', 'value3');
+    map.set('b', 'value4');
+    map.set('2', 'value5');
+
+    const result = mapKeysToArray(map);
+
+    expect(result).toEqual(['a', 'c', '1', 'b', '2']);
+  });
+
+  it('should create empty array from new map', () => {
+    const map = new Map();
+
+    const result = mapKeysToArray(map);
+
+    expect(result).toEqual([]);
   });
 });
