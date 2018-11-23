574ce2be8243e00cd4f503b152c3194fb9033a06
Bill Sorensen <baldgoth@gmail.com>
2018-11-23 15:19:00 -0600

    Print dots, switch PDF to mm

--- src/utils/exportPdf.js
+++ src/utils/exportPdf.js
@@ -1,13 +1,29 @@
 import jsPDF from 'jspdf';
 import * as simple from '../selectors/simple';
 
+const sizeUSLetter = [279.4, 215.9];
+const defaultLineWidth = 0.2;
+const dotRadius = 1.5;
+const dotSpacing = 0.75;
+
+const printDots = (doc, dots, maxDots, x, y) => {
+  doc.setLineWidth(defaultLineWidth);
+
+  for (let i = 0; i < maxDots; i++) {
+    const xOffset = dotRadius + i * dotRadius * 2 + i * dotSpacing;
+    const style = i < dots ? 'FD' : 'S';
+    doc.circle(x + xOffset, y, dotRadius, style);
+  }
+};
+
 const exportPdf = state => {
   const doc = new jsPDF({
-    unit: 'in',
-    format: [11, 8.5] // US Letter; eventually should allow A4
+    unit: 'mm',
+    format: sizeUSLetter // eventually should allow A4
   });
 
   doc.text(simple.getArchetype(state), 1, 1);
+  printDots(doc, 5, 10, 0, 50);
 
   // Downloads
   doc.save('character.pdf');
