2f2cd093606291d559b160e198488ddf1a878a5d
Bill Sorensen <baldgoth@gmail.com>
2018-11-24 14:33:09 -0600

    Print columns and attributes

--- src/utils/exportPdf.js
+++ src/utils/exportPdf.js
@@ -1,5 +1,11 @@
 import jsPDF from 'jspdf';
+import getDots from './getDots';
+import { capitalizeFirstLetter } from './stringUtils';
 import * as simple from '../selectors/simple';
+import {
+  attributeTraitNames,
+  attributeMaxDots
+} from '../constants/characterOptions';
 
 // Units are mm
 const sizeUSLetter = [279.4, 215.9];
@@ -11,13 +17,18 @@ const defaultFont = 'times';
 const defaultFontSize = 10;
 const defaultDrawLineWidth = 0.2;
 const defaultPageLineHeight = 7;
-const dotRadius = 1.5;
+const dotRadius = 1.2;
 const dotSpacing = 0.75;
 let currentYPosition;
 
 const moveToNextLine = () => (currentYPosition += defaultPageLineHeight);
 
-const pointsToMm = points => points / 2.835;
+const getColumnXPosition = columnNumber =>
+  leftMargin + (columnWidth + gutter) * (columnNumber - 1);
+
+const column1XPosition = getColumnXPosition(1);
+const column2XPosition = getColumnXPosition(2);
+const column3XPosition = getColumnXPosition(3);
 
 const print = (doc, text, x) => {
   doc.setFont(defaultFont);
@@ -34,27 +45,24 @@ const printLine = (doc, text, x) => {
 const printDots = (doc, dots, maxDots, x, y) => {
   doc.setLineWidth(defaultDrawLineWidth);
 
+  const yOffset = -dotRadius;
+
   for (let i = 0; i < maxDots; i++) {
     const xOffset = dotRadius + i * dotRadius * 2 + i * dotSpacing;
     const style = i < dots ? 'FD' : 'S';
-    doc.circle(x + xOffset, y, dotRadius, style);
+    doc.circle(x + xOffset, y + yOffset, dotRadius, style);
   }
 };
 
 const getDotsWidth = maxDots =>
   dotRadius * 2 * maxDots + dotSpacing * (maxDots - 1);
 
-const printTrait = (doc, displayName, dots, maxDots, x, y) => {
-  doc.setFont(defaultFont);
-  doc.setFontType('normal');
-  doc.setFontSize(defaultFontSize);
-  doc.text(displayName, x, y);
-
+const printTrait = (doc, displayName, dots, maxDots, x) => {
   const dotsWidth = getDotsWidth(maxDots);
   const xOffset = columnWidth - dotsWidth;
-  const yOffset = -pointsToMm(doc.getLineHeight() / 2) + dotRadius;
 
-  printDots(doc, dots, maxDots, x + xOffset, y + yOffset);
+  print(doc, displayName, x);
+  printDots(doc, dots, maxDots, x + xOffset, currentYPosition);
 };
 
 const exportPdf = state => {
@@ -68,17 +76,26 @@ const exportPdf = state => {
   const bloodline = simple.getBloodline(state);
   const clan = simple.getClanName(state) + (bloodline ? ` (${bloodline})` : '');
 
-  printLine(doc, 'Player:', leftMargin);
-  printLine(doc, 'Character:', leftMargin);
-  printLine(doc, 'Archetype: ' + simple.getArchetype(state), leftMargin);
-  print(doc, 'Clan: ' + clan, leftMargin);
-  print(
-    doc,
-    'Setting/Sect: ' + simple.getSettingName(state),
-    leftMargin + columnWidth + gutter
-  );
-  printLine(doc, 'Title:', leftMargin + (columnWidth + gutter) * 2);
-  printTrait(doc, 'This is a test', 4, 5, 0, 50);
+  printLine(doc, 'Player:', column1XPosition);
+  printLine(doc, 'Character:', column1XPosition);
+  printLine(doc, 'Archetype: ' + simple.getArchetype(state), column1XPosition);
+  print(doc, 'Clan: ' + clan, column1XPosition);
+  print(doc, 'Setting/Sect: ' + simple.getSettingName(state), column2XPosition);
+  printLine(doc, 'Title:', column3XPosition);
+
+  const attributes = simple.getAttributes(state);
+
+  for (let i = 0; i < attributeTraitNames.length; i++) {
+    const name = attributeTraitNames[i];
+
+    printTrait(
+      doc,
+      capitalizeFirstLetter(name),
+      getDots(attributes[name]),
+      attributeMaxDots,
+      getColumnXPosition(i + 1)
+    );
+  }
 
   // Downloads
   doc.save('character.pdf');
