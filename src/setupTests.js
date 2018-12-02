7168aadf78b964bd36fcf87d8872c4f16ee4a124
Bill Sorensen <baldgoth@gmail.com>
2018-12-01 22:19:01 -0600

    Restore and fix deleted test

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// Avoid Jest warning due to loading jspdf
+HTMLCanvasElement.prototype.getContext = () => {};
