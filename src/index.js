73f8986a9681de7b276ce8c12ce5e07ad0108c92
Bill Sorensen <baldgoth@gmail.com>
2018-12-27 19:42:59 -0600

    Remove unused React Router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from './store/configureStore';
 import './styles/index.css';
 import App from './components/App';
@@ -11,9 +10,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
