936ac4b3c1790de676eced9eb79fa16cdc2f03fc
Bill Sorensen <baldgoth@gmail.com>
2018-12-28 21:40:12 -0600

    Disable jsdom for tests, exclude App rendering test
    By skipping a low-value test of App, we avoid loading jspdf,
    which means we don't need jsdom.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-// Avoid Jest warning due to loading jspdf
-HTMLCanvasElement.prototype.getContext = () => {};
