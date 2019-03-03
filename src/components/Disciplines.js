261f67751e3af24e0f201682c05d63f683e07f4c
Bill Sorensen <baldgoth@gmail.com>
2019-03-03 10:18:32 -0600

    Rewrite Disciplines as function component

--- src/components/Disciplines.js
+++ src/components/Disciplines.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TraitCategory from './TraitCategory';
 
@@ -7,48 +7,42 @@ const categoryNameByAffinity = {
   outOfClan: 'Out-of-clan Disciplines'
 };
 
-class Disciplines extends Component {
-  static propTypes = {
-    affinity: PropTypes.oneOf(['inClan', 'outOfClan']).isRequired,
-    names: PropTypes.arrayOf(PropTypes.string).isRequired,
-    displayNameOverride: PropTypes.object.isRequired,
-    traits: PropTypes.object.isRequired,
-    setStartingDots: PropTypes.func.isRequired,
-    purchaseOrUnpurchaseDot: PropTypes.func.isRequired
+export default function Disciplines({
+  affinity,
+  names,
+  displayNameOverride,
+  traits,
+  setStartingDots,
+  purchaseOrUnpurchaseDot
+}) {
+  const handleSetStartingDots = (categoryName, trait, startingDots) => {
+    setStartingDots('disciplines.' + affinity, trait, startingDots);
   };
 
-  handleSetStartingDots = (categoryName, trait, startingDots) => {
-    this.props.setStartingDots(
-      'disciplines.' + this.props.affinity,
-      trait,
-      startingDots
-    );
+  const handlePurchaseOrUnpurchaseDot = (categoryName, trait) => {
+    purchaseOrUnpurchaseDot('disciplines.' + affinity, trait);
   };
 
-  handlePurchaseOrUnpurchaseDot = (categoryName, trait) => {
-    this.props.purchaseOrUnpurchaseDot(
-      'disciplines.' + this.props.affinity,
-      trait
-    );
-  };
+  const adjustAvailable = availableStartingDots => availableStartingDots;
 
-  render() {
-    const { affinity, names, displayNameOverride, traits } = this.props;
-
-    const adjustAvailable = availableStartingDots => availableStartingDots;
-
-    return (
-      <TraitCategory
-        categoryName={categoryNameByAffinity[affinity]}
-        traitNames={names}
-        traitDisplayNameOverride={displayNameOverride}
-        categoryTraits={traits}
-        adjustAvailable={adjustAvailable}
-        setStartingDots={this.handleSetStartingDots}
-        purchaseOrUnpurchaseDot={this.handlePurchaseOrUnpurchaseDot}
-      />
-    );
-  }
+  return (
+    <TraitCategory
+      categoryName={categoryNameByAffinity[affinity]}
+      traitNames={names}
+      traitDisplayNameOverride={displayNameOverride}
+      categoryTraits={traits}
+      adjustAvailable={adjustAvailable}
+      setStartingDots={handleSetStartingDots}
+      purchaseOrUnpurchaseDot={handlePurchaseOrUnpurchaseDot}
+    />
+  );
 }
 
-export default Disciplines;
+Disciplines.propTypes = {
+  affinity: PropTypes.oneOf(['inClan', 'outOfClan']).isRequired,
+  names: PropTypes.arrayOf(PropTypes.string).isRequired,
+  displayNameOverride: PropTypes.object.isRequired,
+  traits: PropTypes.object.isRequired,
+  setStartingDots: PropTypes.func.isRequired,
+  purchaseOrUnpurchaseDot: PropTypes.func.isRequired
+};
