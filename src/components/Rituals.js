713116cd5ba1df48eeac671f2af9d3a8b828fa15
Bill Sorensen <baldgoth@gmail.com>
2019-03-24 08:40:21 -0500

    Replace string parsing with JSON in Rituals

--- src/components/Rituals.js
+++ src/components/Rituals.js
@@ -7,27 +7,24 @@ export default function Rituals({
   updateRituals
 }) {
   const handleChange = e => {
-    // TODO: Move to utils, add tests
-    const stringToArray = s =>
-      s
-        .split(',')
-        .filter(x => x)
-        .map(x => parseInt(x, 10));
-
-    updateRituals(stringToArray(e.target.value));
+    updateRituals(JSON.parse(e.target.value));
   };
 
-  const options = ritualPermutations.map(p => (
-    <option value={p.value.toString()} key={p.value.toString()}>
-      {p.description}
-    </option>
-  ));
+  const options = ritualPermutations.map(p => {
+    const valueAsString = JSON.stringify(p.value);
+
+    return (
+      <option value={valueAsString} key={valueAsString}>
+        {p.description}
+      </option>
+    );
+  });
 
   return (
     <div className="row">
       <div className="col-sm-3">Rituals:</div>
       <div className="col-sm-9">
-        <select value={rituals.toString()} onChange={handleChange}>
+        <select value={JSON.stringify(rituals)} onChange={handleChange}>
           {options}
         </select>
       </div>
